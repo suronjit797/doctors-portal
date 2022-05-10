78f86dffb17ba5d405fad9a36f581653782f1254
Suronjit pal <suronjit797@gmail.com>
2022-05-10 11:39:47 +0600

    first commit

---
+++ src/Pages/Shared/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+
+    const menuItems = (
+        <>
+            <li><NavLink className='.active:bg-neutral' to='/'>Home</NavLink></li>
+            <li><NavLink className='.active:bg-neutral' to='/about'>About</NavLink></li>
+            <li><NavLink className='.active:bg-neutral' to='/appointment'>Appointment</NavLink></li>
+            <li><NavLink className='.active:bg-neutral' to='/review'>Review</NavLink></li>
+            <li><NavLink className='.active:bg-neutral' to='/contact'>Contact Us</NavLink></li>
+            <li><NavLink className='.active:bg-neutral' to='/login'>Login</NavLink></li>
+        </>
+    )
+
+
+    return (
+        <div class="navbar bg-base-100 px-14">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <label tabIndex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to='/' class="normal-case text-xl"> Doctors Postal </Link>
+            </div>
+            <div class="navbar-end hidden lg:flex">
+                <ul class="menu menu-horizontal p-0">
+                    {menuItems}
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
