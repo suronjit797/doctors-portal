233b73c4939b10b74f1fffa2daf54b32214a5c93
Suronjit pal <suronjit797@gmail.com>
2022-05-11 21:05:15 +0600

    appointment

--- src/Pages/Appointment/AppointmentService.js
+++ src/Pages/Appointment/AppointmentService.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import AppointmentServiceCard from './AppointmentServiceCard';
+import axios from 'axios';
 
 
 const AppointmentService = ({ dt }) => {
     const date = dayjs(dt).format('MMMM DD, YYYY')
+    const [services, setServices] = useState([])
 
-    const homeInfo = []
+    useEffect(() => {
+        axios.get('service.json')
+            .then(res => setServices(res.data))
+    }, [])
+
     return (
         <div>
             <h2 className="text-center text-primary text-xl my-10 md:my-20 font-bold"> Available Appointments on {date} </h2>
 
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-4 my-10'>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-7 my-10'>
                 {
-                    homeInfo.map(info => <AppointmentServiceCard cardInfo={info} />)
+                    services.map(info => <AppointmentServiceCard key={info._id} cardInfo={info} />)
                 }
             </div>
         </div>
--- src/Pages/Appointment/AppointmentServiceCard.js
+++ src/Pages/Appointment/AppointmentServiceCard.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
-const AppointmentServiceCard = ({cardInfo}) => {
-    const { image, title, description, bg } = cardInfo
+const AppointmentServiceCard = ({ cardInfo }) => {
+    const { name, slots } = cardInfo
+    console.log(cardInfo);
     return (
-        <div className={`card lg:card-side p-6 text-white shadow-xl ${bg}`}>
-            <figure>
-                <img src={image} alt="" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title"> {title} </h2>
-                <p> {description} </p>
+        <div className={`card  p-6 text-center shadow-md`}>
+            <div className='flex flex-col min-h-full'>
+                <h2 className="text-primary text-xl mb-6 font-bold"> {name} </h2>
+
+                <div className="mb-4">
+                    {
+                        slots.map((slot, index) => (
+                            <p className='mb-2'> {slot} </p>
+                        ))
+                    }
+                </div>
+
+                <button
+                    className="btn btn-primary text-white mx-auto mt-auto"
+                    disabled={!slots.length}
+                > Book Appointment </button>
             </div>
         </div>
     );
